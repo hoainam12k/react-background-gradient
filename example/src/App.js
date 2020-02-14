ba37558fde0e57d313bef9506dad579f849b4fae
Neo Khuat <hoainam12k@gmail.com>
2020-02-14 11:24:42 +0700

    change example

--- example/src/App.js
+++ example/src/App.js
@@ -7,32 +7,40 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      range: [
-         {
-          offsetX: 15,
-          r: 34,
-          g: 27,
-          b: 32,
-          a: 1,
-          hex: '#221B20'
-        },
-         {
-          offsetX: 50,
-          r: 134,
-          g: 57,
-          b: 62,
-          a: 1,
-          hex: '#86393E'
-        }
-      ],
-      angle: 0
+      gradient: {
+        range: [
+          {
+            offsetX: 15,
+            r: 34,
+            g: 27,
+            b: 32,
+            a: 1,
+            hex: '#221B20'
+          },
+          {
+            offsetX: 50,
+            r: 134,
+            g: 57,
+            b: 62,
+            a: 1,
+            hex: '#86393E'
+          }
+        ],
+        angle: 0,
+        type: 'linear',
+        palettle: Palettle
+      }
     }
+  }
+
+  testChange = () => {
+    console.log('object');
   }
 
   render() {
     return (
       <div>
-        <ExampleComponent range={this.state.range} angle={this.state.angle} palettle={Palettle} />
+        <ExampleComponent gradient={this.state.gradient} onChange={this.testChange.bind(this)} />
       </div>
     )
   }
